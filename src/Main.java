338f1100cc8a6c0b83405fa26f111c366850e54e
abalmasDA <abalmasit@gmail.com>
2023-06-19 17:49:44 +0300

    Commit1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Створіть проект за допомогою IntelliJ IDEA. Потрібно: Описати клас з іменем Price, що містить такі поля:
+ *  назву товару;
+ *  назву магазину, в якому продається товару
+ *  вартість товару у гривнях.
+ * Написати програму, яка виконує такі дії:
+ *  введення з клавіатури даних до масиву, що складається з двох елементів типу Price (записи мають бути впорядковані в алфавітному порядку за назвами магазинів);
+ *  виведення на екран інформації про товари, що продаються в магазині, назва якого введена з клавіатури (якщо такого магазину немає, вивести виняток).
+ */
+
+public class Main {
+    public static Scanner scannerInputData = new Scanner(System.in);
+    private static int numberElements = 2;
+    private ArrayList<Price> ListPrices = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < numberElements - 1; i++) {
+
+            System.out.println();
+
+
+        }
+
+
+    }
+
+
 }
--- src/Price.java
+++ src/Price.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Price {
+public class Price {
+    private String storeName;
+    private String productName;
+    private Double productPrice;
+
+    public Price(String storeName, String productName, Double productPrice) {
+        this.storeName = storeName;
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
 }
