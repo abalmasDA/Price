ef436c8ba8c3ad02912074433e9344d59104e045
abalmasDA <abalmasit@gmail.com>
2023-06-22 23:05:54 +0300

    Commit2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 /**
@@ -14,20 +16,49 @@ import java.util.Scanner;
 public class Main {
     public static Scanner scannerInputData = new Scanner(System.in);
     private static int numberElements = 2;
-    private ArrayList<Price> ListPrices = new ArrayList<>();
-
+    private static ArrayList<Price> ListPrices = new ArrayList<>();
 
     public static void main(String[] args) {
-
-        for (int i = 0; i < numberElements - 1; i++) {
-
-            System.out.println();
-
-
+        System.out.println("Enter data for" + numberElements + " products:");
+        for (int i = 0; i < numberElements; i++) {
+            System.out.println("product " + (i + 1) + ":");
+            System.out.print("Name of product: ");
+            String productName = scannerInputData.nextLine();
+            System.out.print("Name of store: ");
+            String storeName = scannerInputData.nextLine();
+            System.out.print("Price of product: ");
+            double priceValue = scannerInputData.nextDouble();
+            scannerInputData.nextLine();
+            Price newPrice = new Price(productName, storeName, priceValue);
+            ListPrices.add(newPrice);
         }
 
+        Collections.sort(ListPrices, Comparator.comparing(Price::getStoreName));
+        System.out.print("Enter the store name to search: ");
+        String searchStore = scannerInputData.nextLine();
+        System.out.println(searchStore);
+        getInfoProductByStore(searchStore);
 
     }
-
-
+    public static void getInfoProductByStore(String searchStore) {
+        for (int i = 0; i < ListPrices.size(); i++) {
+            if (ListPrices.get(i).getStoreName().equalsIgnoreCase(searchStore)) {
+                System.out.println("Product: " + ListPrices.get(i).getProductName());
+                System.out.println("Store: " + ListPrices.get(i).getStoreName());
+                System.out.println("Price: " + ListPrices.get(i).getProductPrice());
+                break;
+            }else{
+                throw new RuntimeException("no such store found");
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
