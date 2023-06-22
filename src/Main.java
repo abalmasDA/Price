7245513647f18c9a38c728ad1c2c87ac3d700a7c
Andrii Boiko <andrii.m.boiko@gmail.com>
2023-06-22 23:31:49 +0300

    Minor fixes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -14,12 +14,14 @@ import java.util.Scanner;
  */
 
 public class Main {
-    public static Scanner scannerInputData = new Scanner(System.in);
-    private static int numberElements = 2;
-    private static ArrayList<Price> ListPrices = new ArrayList<>();
+
 
     public static void main(String[] args) {
+        Scanner scannerInputData = new Scanner(System.in);
+        int numberElements = 2;
+        List<Price> listPrices = new ArrayList<>();
         System.out.println("Enter data for" + numberElements + " products:");
+
         for (int i = 0; i < numberElements; i++) {
             System.out.println("product " + (i + 1) + ":");
             System.out.print("Name of product: ");
@@ -30,26 +32,59 @@ public class Main {
             double priceValue = scannerInputData.nextDouble();
             scannerInputData.nextLine();
             Price newPrice = new Price(productName, storeName, priceValue);
-            ListPrices.add(newPrice);
+            listPrices.add(newPrice);
         }
 
-        Collections.sort(ListPrices, Comparator.comparing(Price::getStoreName));
+        listPrices.sort(Comparator.comparing(Price::getStoreName));
         System.out.print("Enter the store name to search: ");
         String searchStore = scannerInputData.nextLine();
         System.out.println(searchStore);
-        getInfoProductByStore(searchStore);
+        getInfoProductByStore(searchStore, listPrices);
 
     }
-    public static void getInfoProductByStore(String searchStore) {
-        for (int i = 0; i < ListPrices.size(); i++) {
-            if (ListPrices.get(i).getStoreName().equalsIgnoreCase(searchStore)) {
-                System.out.println("Product: " + ListPrices.get(i).getProductName());
-                System.out.println("Store: " + ListPrices.get(i).getStoreName());
-                System.out.println("Price: " + ListPrices.get(i).getProductPrice());
-                break;
-            }else{
-                throw new RuntimeException("no such store found");
+
+    public static void getInfoProductByStore(String searchStore, List<Price> listPrices) {
+        /*
+        після сортування у Вас не гарантована послідовність
+        і одразу на першій ітерації Ви могли просто пропускати Ваш магазин
+        не можна робити все в одній ітерації
+        треба перевірити весь список
+        і потім виводи дані - є такий магазин чи нема
+         */
+
+
+//        for (int i = 0; i < listPrices.size(); i++) {
+//            if (listPrices.get(i).getStoreName().equalsIgnoreCase(searchStore)) {
+//                System.out.println("Product: " + listPrices.get(i).getProductName());
+//                System.out.println("Store: " + listPrices.get(i).getStoreName());
+//                System.out.println("Price: " + listPrices.get(i).getProductPrice());
+//                break;
+//            } else {
+//                throw new RuntimeException("no such store found");
+//            }
+//        }
+
+
+        /*
+        треба щось таке
+        тому що у Вас може бути декілька магазинів з однією назвою
+        то ми всіх їх і показуємо
+
+        а якщо не знайшли - кидаємо Exception
+        до речі краще створити свій власний Exception замість RuntimeException
+         */
+
+        boolean isNotFound = true;
+        for (Price listPrice : listPrices) {
+            if (listPrice.getStoreName().equalsIgnoreCase(searchStore)) {
+                System.out.println("Product: " + listPrice.getProductName());
+                System.out.println("Store: " + listPrice.getStoreName());
+                System.out.println("Price: " + listPrice.getProductPrice());
+                isNotFound = false;
             }
+        }
+        if (isNotFound) {
+            throw new RuntimeException("No such store found");
         }
     }
 }
--- src/Price.java
+++ src/Price.java
@@ -1,11 +1,19 @@
 public class Price {
-    private String storeName;
-    private String productName;
-    private Double productPrice;
+    private final String storeName;
+    private final String productName;
+    private final Double productPrice;
 
-    public Price(String storeName, String productName, Double productPrice) {
-        this.storeName = storeName;
+    public Price(String productName, String storeName, Double productPrice) {
+        /*
+        тут також помилка
+        Ви передаєте - new Price(productName, storeName, priceValue)
+        а зберігаєте  productName --> storeName
+         */
+//        this.storeName = storeName;
+//        this.productName = productName;
+//        this.productPrice = productPrice;
         this.productName = productName;
+        this.storeName = storeName;
         this.productPrice = productPrice;
     }
 
